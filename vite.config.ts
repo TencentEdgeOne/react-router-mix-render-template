32fb95fcb9a22bcb1b130a3e7d22de9666482ce2
chuckcchen <chuckcchen@tencent.com>
2025-11-05 11:23:11 +0800

    feat: Update npm packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { edgeoneAdapter } from "@chuckcchen/react-router-pages";
+import { edgeoneAdapter } from "@tencent/react-router-edgeone";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), edgeoneAdapter()],
